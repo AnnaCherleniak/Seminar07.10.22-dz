376d7ee7fbfaff55044c8ccf023351645750f693
Anna <ankakras@yandex.ru>
2022-10-13 13:36:39 +0700

    Решение задачи 004

---
+++ Task004/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру.
+
+Console.Write("Введите значение N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+for(int i = 2; i <= N; i= i+2)
+{
+    Console.WriteLine($"{i} ^3 = {i*i*i}");
+}
