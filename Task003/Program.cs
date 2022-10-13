5528c6e47f86a67d2850dd562aae6fecbc2b1372
Anna <ankakras@yandex.ru>
2022-10-13 12:52:47 +0700

    Решение задачи 003

---
+++ Task003/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N.
+Console.Write("Введите значение N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int sum = 1;
+for(int i = 1; i <= N; i++)
+{
+    sum = sum*i;
+}    
+Console.WriteLine("Произведение чисел от 1 до N = " +sum);
+
