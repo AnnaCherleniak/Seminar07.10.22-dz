1f02f97493b2aec606bcc02fc69cabe8341630cc
Anna <ankakras@yandex.ru>
2022-10-13 13:08:55 +0700

    Решение задачи 002

---
+++ Task002/Program.cs
@@ -0,0 +1,10 @@
+﻿// Подсчитать сумму цифр в числе.
+
+Console.Write("Введите число N - ");
+string N = Convert.ToString(Console.ReadLine() ?? "0");
+int sum = 0;
+for(int i = 0; i<N.Length; i++)
+{
+    sum += Convert.ToInt32(N[i].ToString());
+}
+Console.WriteLine(sum);
